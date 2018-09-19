55bd8457ae996daa10d21418cd8bd30efb20ebfe
unah254 <greisunah@gmail.com>
2018-09-19 21:58:55 +0300

    renamed _init_ file in app folder and set config options to development

--- run.py
+++ run.py
@@ -6,10 +6,10 @@ currentdir = os.path.dirname(os.path.abspath(
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
 
-from app.___init__ import create_app
+from app import create_app
 
 config_name = os.getenv('FLASK_CONFIG')
-app = create_app(config_name)
+app = create_app('development')
 
 
 if __name__ == '__main__':
