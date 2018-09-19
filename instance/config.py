c09edcc69bfc264050d3f9abf911191c7225b54f
unah254 <greisunah@gmail.com>
2018-09-19 20:19:08 +0300

    removed instance folder from gitignore

---
+++ instance/config.py
@@ -0,0 +1,37 @@
+'''Set up environment specific configurations'''
+import os
+
+
+class Config(object):
+    '''Parent configuration class'''
+    DEBUG = False
+    CSRF_ENABLED = True
+
+
+class Development(Config):
+    #Configuration for development
+    DEBUG = True
+
+
+class Testing(Config):
+    '''Configuration for testing'''
+    TESTING = True
+    DEBUG = True
+
+class StagingConfig(Config):
+    """Configuration for Staging."""
+    DEBUG = False
+
+
+class Production(Config):
+    '''Configuration for production'''
+    DEBUG = False
+    TESTING = False
+
+
+app_config = {
+    'development': Development,
+    'testing': Testing,
+    'staging' : StagingConfig,
+    'production': Production
+}
