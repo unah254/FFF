de33a36950d5a2415ec004719912368c1ad0e622
unah254 <greisunah@gmail.com>
2018-09-27 22:00:28 +0300

    [feature] redefined update status method

--- app/api/v1/validators.py
+++ app/api/v1/validators.py
@@ -3,10 +3,12 @@ import re
 
 class Validators:
     def valid_food_name(self, name):
+        '''confirming name input has numbers and letters only'''
         regex = "^[a-zA-Z0-9_ ]+$"
         return re.match(regex, name)
 
 
     def valid_food_description(self, description):
+        '''confirming description has numbers and letters only'''
         regex = "^[a-zA-Z0-9_ ]+$"
         return re.match(regex, description)
--- app/api/v1/views.py
+++ app/api/v1/views.py
@@ -1,21 +1,25 @@
+# imported necessary modules to create flask microframework
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
-
+# imported module to validate the inputs
 from .validators import Validators
 
-
+# list to store orders
 orders = []
 
+
 class Mealorder:
-    
-    def __init__(self,name=None,price=None,description=None, status="Pending"):
-        self.id=len(orders)+1
-        self.name=name
-        self.price=price
-        self.description=description
-        self.status=status
-        
+    '''meal order class to initialize orders and display in json'''
+    def __init__(self, name=None, price=None, description=None, status="Pending"):
+        '''create an instance of meal order'''
+        self.id = len(orders)+1
+        self.name = name
+        self.price = price
+        self.description = description
+        self.status = status
+
     def serialize(self):
+        '''to get created data and display in json format'''
         return dict(
             id=self.id,
             name=self.name,
@@ -25,37 +29,39 @@ class Mealorder:
         )
 
     def get_id(self, order_id):
+        '''display specific order id'''
         for order in orders:
             if order.id == order_id:
                 return order
 
+
 class Createmeal(Resource):
+    '''to get input from user and create a new order'''
     parser = reqparse.RequestParser()
     parser.add_argument(
-    'name',
-    type=str,
-    required=True,
-    help="This field cannot be left blank"
+        'name',
+        type=str,
+        required=True,
+        help="This field cannot be left blank"
     )
 
     parser.add_argument(
-    'description',
-    type=str,
-    required=True,
-    help="This field cannot be left blank"
+        'description',
+        type=str,
+        required=True,
+        help="This field cannot be left blank"
     )
 
     parser.add_argument(
         'price',
         type=int,
         required=True,
-        help="This field cannot be left blank!" 
+        help="This field cannot be left blank!"
     )
 
     def post(self):
         ''' place new order'''
         data = Createmeal.parser.parse_args()
-
 
         name = data['name']
         description = data['description']
@@ -70,8 +76,9 @@ class Createmeal(Resource):
 
         orders.append(order)
 
-        return {"message":"Dish order placed"}, 201
-   
+        return {"message": "Dish order placed"}, 201
+
+
 class Orders(Resource):
 
     def get(self):
@@ -79,48 +86,58 @@ class Orders(Resource):
 
         return {'orders': [order.serialize() for order in orders]}, 200
 
+
+class UpdateStatus(Resource):
+    '''class to create status arguments'''
+    parser = reqparse.RequestParser()
+    parser.add_argument(
+        'status',
+        type=str,
+        required=True,
+        help="This field cannot be left blank!"
+    )
+
+
 class SingleOrder(Resource):
-    
+    '''class to get, update and delete specific order'''
+
     def get(self, id):
         ''' get a specific order '''
-        
+
         order = Mealorder().get_id(id)
 
         if order:
-            return {"order":order.serialize()}
-        
-        return {'message':"Not found"}, 404
-        
+            return {"order": order.serialize()}
+
+        return {'message': "Not found"}, 404
+
     def put(self, id):
         ''' Update order status '''
+
         order = Mealorder().get_id(id)
-
+        data = UpdateStatus.parser.parse_args()
+        status = data['status']
         if order:
-            if order.status == "Pending":
+            if status == "Accepted":
                 order.status = "Accepted"
-            
-                return {'message':'Order accepted'}, 200
+                return {'message': 'Order accepted'}, 200
 
-            if order.status == "Accepted":
+            elif status == "Completed":
                 order.status = "Completed"
 
-                return {'message':'Order completed'}, 200
+                return {'message': 'Order completed'}, 200
+            elif status == "Pending" or status == "":
+                return {'message': 'Please update the order status to either Accepted or Completed'}, 400
 
-        else: 
-           return {'message':"Not found"}, 404
+        else:
+            return {'message': "Not found"}, 404
 
-    def delete(self, id):
+
+def delete(self, id):
         ''' Delete a single order '''
 
         order = Mealorder().get_id(order_id=id)
         if order:
             orders.remove(order)
-            return {'message':"Deleted"}, 200
-        return {'message':"Not found"}, 404
-  
-
-
-
-        
-
-
+            return {'message': "Deleted"}, 200
+        return {'message': "Not found"}, 404
